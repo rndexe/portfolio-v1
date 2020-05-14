d071a3e69b4fbe2f57cd6af108f543413f210944
rndexe <h_erande@yahoo.co.in>
2020-05-14 14:10:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
   background(213, 126, 166);
+  fill(57, 58, 61);
+  let marginx = windowWidth/5;
+  let marginy = windowHeight/10; 		
+  rect(marginx,marginy, windowWidth - 2*marginx, windowHeight - 2*marginy);  		
 }
 
 function windowResized() {
