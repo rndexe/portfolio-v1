d49e47eb3d83d9741acfc6b46e56c08fae90e22d
rndexe <h_erande@yahoo.co.in>
2020-05-20 12:41:29 +0530

    website change
    Changed from pink and grey rect to solarized dark random text

--- sketch.js
+++ sketch.js
@@ -1,16 +1,46 @@
+let font, fontsize = 18;
+let brblack, bryellow;
+let w, h;
+
+function preload() {
+  font = loadFont('assets/SourceCodePro.ttf');
+}
+
 function setup() {
+
+  frameRate(6);
   createCanvas(windowWidth, windowHeight);
-  noStroke();
+
+  w = windowWidth;
+  h = windowHeight;
+  brblack = color(0, 43, 54);
+  bryellow = color(101, 123, 131);
+  brred = color(220, 50, 47);
+  fill(bryellow);
+  textAlign(LEFT, TOP);
+  textFont(font);
+  textSize(fontsize);
+
 }
 
 function draw() {
-  background(213, 126, 166);
-  fill(57, 58, 61);
-  let marginx = windowWidth/5;
-  let marginy = windowHeight/10; 		
-  rect(marginx,marginy, windowWidth - 2*marginx, windowHeight - 2*marginy);  		
+  background(brblack);
+
+  let letter = char(random(48, 122));
+
+  fill(bryellow);
+  for (let i = 0; i < w; i += fontsize * 0.6)
+    for (let j = 0; j < h; j += fontsize) {
+      text(char(random(33, 126)), i, j);
+    }
+
+  print(frameRate());
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  w = windowWidth;
+  h = windowHeight;
+  print(w);
+  print(h);
 }
